b09075f08bf1593214afc84870b29db456df0b04
Victor de Baare <victor.de.baare@gmail.com>
2020-11-23 11:25:34 +0100

    Update

--- TestBuilder/TestBuilder/GenerateTestBuilder.cs
+++ TestBuilder/TestBuilder/GenerateTestBuilder.cs
@@ -14,8 +14,8 @@ namespace TestBuilder
         public override bool Execute()
         {
             Log.LogMessage(MessageImportance.High, "Test2");
-            
-            if(Config != null) 
+
+            if (Config != null)
             {
                 Log.LogMessage(MessageImportance.High, string.Join(",", Config.Select(x => x.ItemSpec)));
             }
@@ -23,89 +23,71 @@ namespace TestBuilder
             {
                 Log.LogMessage(MessageImportance.High, "No config files found");
 
-                var assembly = Assembly.Load(AssemblyName);
 
-                if(assembly == null)
-                {
-                    Log.LogMessage(MessageImportance.High, "Assembly not loaded");
-                }
-                else
-                {
-                    var configs = assembly.GetTypes().Where(x => x.IsClass && typeof(ITestEntityBuilderConfig).IsAssignableFrom(x));
-                    var writer = new FileWriter(Log);
+                var writer = new FileWriter(Log);
 
-                    if(configs != null && configs.Any())
-                    {
-                        Log.LogMessage(MessageImportance.High, "Implementation Found");
+                //foreach (var config in configs)
+                //{
+                //    var item = (ITestEntityBuilderConfig)Activator.CreateInstance(config);
 
-                        foreach (var config in configs)
-                        {
-                            var item = (ITestEntityBuilderConfig)Activator.CreateInstance(config);
+                //    foreach (var typesToGenerate in item.Configs)
+                //    {
+                //        var stringBuilder = new IndentedStringBuilder();
 
-                            foreach(var typesToGenerate in item.Configs)
-                            {
-                                var stringBuilder = new IndentedStringBuilder();
+                //        stringBuilder.AppendLine($"namespace {item.Namespace}")
+                //            .AppendLine("{");
 
-                                stringBuilder.AppendLine($"namespace {item.Namespace}")
-                                    .AppendLine("{");
+                //        using (stringBuilder.Indent())
+                //        {
+                //            stringBuilder.AppendLine($"using {typesToGenerate.Namespace};")
+                //                .AppendLine($"using System;")
+                //                .AppendLine($"using System.Linq;")
+                //                .AppendLine($"public partial class {typesToGenerate.Name}Builder")
+                //                .AppendLine("{");
 
-                                using (stringBuilder.Indent())
-                                {
-                                    stringBuilder.AppendLine($"using {typesToGenerate.Namespace};")
-                                        .AppendLine($"using System;")
-                                        .AppendLine($"using System.Linq;")
-                                        .AppendLine($"public partial class {typesToGenerate.Name}Builder")
-                                        .AppendLine("{");
+                //            using (stringBuilder.Indent())
+                //            {
+                //                stringBuilder.AppendLine($"private {typesToGenerate.Name} _entity");
+                //                stringBuilder.AppendLine();
+                //                stringBuilder.AppendLine($"private {typesToGenerate.Name}Builder()")
+                //                    .AppendLine("{");
 
-                                    using (stringBuilder.Indent())
-                                    {
-                                        stringBuilder.AppendLine($"private {typesToGenerate.Name} _entity");
-                                        stringBuilder.AppendLine();
-                                        stringBuilder.AppendLine($"private {typesToGenerate.Name}Builder()")
-                                            .AppendLine("{");
+                //                using (stringBuilder.Indent())
+                //                {
+                //                    stringBuilder.AppendLine($"_entity = new {typesToGenerate}();");
+                //                    stringBuilder.AppendLine($"InitializeEntity(_entity);");
+                //                }
 
-                                        using (stringBuilder.Indent())
-                                        {
-                                            stringBuilder.AppendLine($"_entity = new {typesToGenerate}();");
-                                            stringBuilder.AppendLine($"InitializeEntity(_entity);");
-                                        }
+                //                stringBuilder.AppendLine("}");
+                //                stringBuilder.AppendLine();
+                //                stringBuilder.AppendLine($"partial InitializeEntity({typesToGenerate.Name} entity);");
 
-                                        stringBuilder.AppendLine("}");
-                                        stringBuilder.AppendLine();
-                                        stringBuilder.AppendLine($"partial InitializeEntity({typesToGenerate.Name} entity);");
+                //                stringBuilder.AppendLine($"public static {typesToGenerate.Name}Builder Valid()")
+                //                    .AppendLine("{");
 
-                                        stringBuilder.AppendLine($"public static {typesToGenerate.Name}Builder Valid()")
-                                            .AppendLine("{");
+                //                using (stringBuilder.Indent())
+                //                {
+                //                    stringBuilder.AppendLine($"return new {typesToGenerate.Name}Builder()");
+                //                }
 
-                                        using (stringBuilder.Indent())
-                                        {
-                                            stringBuilder.AppendLine($"return new {typesToGenerate.Name}Builder()");
-                                        }
+                //                stringBuilder.AppendLine("}");
+                //            }
 
-                                        stringBuilder.AppendLine("}");
-                                    }
+                //            stringBuilder.AppendLine("}");
+                //        }
 
-                                    stringBuilder.AppendLine("}");
-                                }
+                //        stringBuilder.AppendLine("}");
 
-                                stringBuilder.AppendLine("}");
+                //        var file = new CodeFile
+                //        {
+                //            GeneratedCode = stringBuilder.ToString(),
+                //            Path = Path.GetDirectoryName(ProjectPath),
+                //        };
 
-                                var file = new CodeFile
-                                {
-                                    GeneratedCode = stringBuilder.ToString(),
-                                    Path = Path.GetDirectoryName(ProjectPath),
-                                };
+                //        writer.WriteOutputToFile(file);
+                //    }
+                //}
 
-                                writer.WriteOutputToFile(file);
-                            }
-                        }
-                        
-                    }
-                    else
-                    {
-                        Log.LogMessage(MessageImportance.High, "No implementations found");
-                    }
-                }
             }
 
             Log.LogMessage(MessageImportance.High, $"{ProjectPath}");
@@ -115,6 +97,9 @@ namespace TestBuilder
         }
 
         public ITaskItem[] Config { get; set; }
+
+        [Output]
+        public ITaskItem[] GeneratedFiles { get; private set; }
 
         [Required]
         public string ProjectPath { get; set; }
